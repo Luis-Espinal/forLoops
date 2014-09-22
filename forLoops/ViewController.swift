1a268a52d4d00283c85ee1a1434f43af3e9b3318
Luis Espinal <lamespinal@Luiss-MacBook-Pro.local>
2014-09-22 12:50:05 -0400

    first commit

--- forLoops/ViewController.swift
+++ forLoops/ViewController.swift
@@ -13,6 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+//        var meditationHours = 1
+//        for meditationHours; meditationHours < 100; meditationHours = meditationHours + 1 {
+//            println("I am getting more enlightened")
+//            
+//        }
+//  
+//        for var meditationHours = 1; meditationHours < 100; meditationHours++
+//        {
+//            println("I am getting more enlightened \(meditationHours)")
+//        }
+//        
+       
+//        var wheat = 1
+//        
+//        for var i = 0; i < 64; ++i {
+//            wheat = wheat * 2
+//            println("locations on board \(i) and pieces of wheat: \(wheat)")
+//        }
+        
+        
+        for var bottlesOfSoda = 99; bottlesOfSoda >= 0; --bottlesOfSoda {
+            if bottlesOfSoda == 0 {
+                println("No more bottles of soda on the wall")
+            } else {
+                println("\(bottlesOfSoda) bottles of soda on the wall")
+            }
+        }
+            
     }
 
     override func didReceiveMemoryWarning() {
